9c11dc6ac4f5e958df5ab21ec9f879c41aab06ea
Nate Wilcox <nathan+dev@electriccoin.co>
2020-11-05 15:48:59 +0900

    Import directories without `default.nix` as "attrset imports".

--- src/default.nix
+++ src/default.nix
@@ -1,11 +1,11 @@
 let
-  inherit (builtins) pathExists typeOf;
+  inherit (builtins) pathExists readDir typeOf;
   inherit (import <nixpkgs> {}) lib;
   inherit (lib.strings) hasPrefix removePrefix;
 
   mkImp = root: dir: impSpec:
-    assert typeOf root == "path";
-    assert typeOf dir == "path";
+    assert fsKind root == "directory";
+    assert fsKind dir == "directory";
     assert typeOf impSpec == "string";
 
     if hasPrefix "/" impSpec
@@ -13,19 +13,56 @@ let
     else impFrom root dir impSpec;
 
   impFrom = root: base: impSpec:
+    assert fsKind root == "directory";
+    assert fsKind base == "directory";
+    assert typeOf impSpec == "string";
+
     let
       subp = base + "/${impSpec}";
       fileCandidate = subp + ".nix";
-      dirCandidate = subp + "/default.nix";
+      defCandidate = subp + "/default.nix";
     in if pathExists fileCandidate
     then imp root (dirOf fileCandidate) fileCandidate
-    else imp root dirCandidate dirCandidate;
+    else if pathExists defCandidate
+    then imp root subp defCandidate
+    else impDirSet root subp;
 
   imp = root: dir: impPath:
-    assert typeOf root == "path";
-    assert typeOf dir == "path";
+    assert fsKind root == "directory";
+    assert fsKind dir == "directory";
     assert typeOf impPath == "path";
     import impPath (mkImp root dir);
+
+  impDirSet = root: dir:
+    assert fsKind root == "directory";
+    assert fsKind dir == "directory";
+    let
+      inherit (lib.attrsets) filterAttrs mapAttrs';
+      inherit (lib.strings) hasSuffix removeSuffix;
+
+      impEntry = n: kind:
+        if kind == "directory" || kind == "regular" && hasSuffix ".nix" n
+        then rec {
+          name = removeSuffix ".nix" n;
+          value = mkImp root dir name;
+        }
+        else {
+          name = n;
+          value = {};
+        };
+
+      unfiltered = mapAttrs' impEntry (readDir dir);
+    in
+      filterAttrs (_: v: v != {}) unfiltered;
+
+  fsKind = p:
+    assert typeOf p == "path";
+    assert pathExists p;
+    let
+      entries = readDir (dirOf p);
+      fname = baseNameOf p;
+    in
+      entries."${fname}";
 
 in
   projDir: mkImp projDir projDir
