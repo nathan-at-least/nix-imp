b349855d5401109c2cbe9a7018728d864fb84869
Nate Wilcox <nathan+dev@electriccoin.co>
2020-11-05 13:04:17 +0900

    Two installer fixes.

--- default.nix
+++ default.nix
@@ -6,6 +6,8 @@ in
     version = "0.1.0";
     src = ./src;
     builder = writeScript "${pname}-${version}-builder.sh" ''
+      source "$stdenv/setup"
       cp -r "$src" "$out"
+      chmod -R u+w "$out" # Workaround for https://github.com/NixOS/nix/pull/3321
     '';
   }
